d93e093e655e52246ac25480bf10a3b678119243
Quinn Murphy <qmurphy64@gmail.com>
2019-12-03 19:14:03 -0600

    Improve main to reduce use of Options

--- src/main.rs
+++ src/main.rs
@@ -2,51 +2,52 @@ mod day1;
 
 struct DayInfo
 {
-    part1_func: Option<fn( &str ) -> String>,
-    part2_func: Option<fn( &str ) -> String>,
-    input_file: Option<&'static str>
+    part1_func: fn( &str ) -> String, // The function pointer to run Part 1 of that day
+    part2_func: fn( &str ) -> String, // The function pointer to run Part 2 of that day
+    input_file: &'static str          // The file to read the input from
 }
 
 const NUM_DAYS : usize = 25;
 
-static DAYS: [DayInfo; NUM_DAYS] =
+static DAYS: [Option<DayInfo>; NUM_DAYS] =
 [
-    DayInfo { part1_func: Some( day1::part1 ), part2_func: Some( day1::part2 ), input_file: Some( "input/1.txt" ) },
-    DayInfo { part1_func: None,                part2_func: None,                input_file: None                  },
-    DayInfo { part1_func: None,                part2_func: None,                input_file: None                  },
-    DayInfo { part1_func: None,                part2_func: None,                input_file: None                  },
-    DayInfo { part1_func: None,                part2_func: None,                input_file: None                  },
-    DayInfo { part1_func: None,                part2_func: None,                input_file: None                  },
-    DayInfo { part1_func: None,                part2_func: None,                input_file: None                  },
-    DayInfo { part1_func: None,                part2_func: None,                input_file: None                  },
-    DayInfo { part1_func: None,                part2_func: None,                input_file: None                  },
-    DayInfo { part1_func: None,                part2_func: None,                input_file: None                  },
-    DayInfo { part1_func: None,                part2_func: None,                input_file: None                  },
-    DayInfo { part1_func: None,                part2_func: None,                input_file: None                  },
-    DayInfo { part1_func: None,                part2_func: None,                input_file: None                  },
-    DayInfo { part1_func: None,                part2_func: None,                input_file: None                  },
-    DayInfo { part1_func: None,                part2_func: None,                input_file: None                  },
-    DayInfo { part1_func: None,                part2_func: None,                input_file: None                  },
-    DayInfo { part1_func: None,                part2_func: None,                input_file: None                  },
-    DayInfo { part1_func: None,                part2_func: None,                input_file: None                  },
-    DayInfo { part1_func: None,                part2_func: None,                input_file: None                  },
-    DayInfo { part1_func: None,                part2_func: None,                input_file: None                  },
-    DayInfo { part1_func: None,                part2_func: None,                input_file: None                  },
-    DayInfo { part1_func: None,                part2_func: None,                input_file: None                  },
-    DayInfo { part1_func: None,                part2_func: None,                input_file: None                  },
-    DayInfo { part1_func: None,                part2_func: None,                input_file: None                  },
-    DayInfo { part1_func: None,                part2_func: None,                input_file: None                  }
+    Some( DayInfo { part1_func: day1::part1, part2_func: day1::part2, input_file: "input/1.txt" } ),
+    None,
+    None,
+    None,
+    None,
+    None,
+    None,
+    None,
+    None,
+    None,
+    None,
+    None,
+    None,
+    None,
+    None,
+    None,
+    None,
+    None,
+    None,
+    None,
+    None,
+    None,
+    None,
+    None,
+    None
 ];
 
 fn main() -> Result<(), i32>
 {
-    // Determine which day to run
+    // Read in which day to run
     println!( "Run which day's code?" );
     let mut which_day_str = String::new();
     let which_day = match std::io::stdin().read_line( &mut which_day_str )
     {
         Ok( _ ) =>
         {
+            // Try to parse the integer
             match which_day_str.trim().parse()
             {
                 Ok( which_day ) => which_day,
@@ -63,14 +64,14 @@ fn main() -> Result<(), i32>
     }
     
     // Index the day info
-    let day_info = &DAYS[which_day - 1];
+    let day = &DAYS[which_day - 1];
 
     // Read in the input
-    match day_info.input_file
+    match day
     {
-        Some( input_file ) =>
+        Some( day_info ) =>
         {
-            let input_str = match std::fs::read_to_string( input_file )
+            let input_str = match std::fs::read_to_string( day_info.input_file )
             {
                 Ok( input_str ) => input_str,
                 Err( _ )   =>
@@ -80,24 +81,16 @@ fn main() -> Result<(), i32>
                 }
             };
             
+            // Execute
             println!( "Executing Day {}", which_day );
-            match day_info.part1_func
-            {
-                Some( f ) => println!( "Part 1 Result: {}", f( &input_str ) ),
-                None => println!( "No function for part 1" )
-            };
-
-            match day_info.part2_func
-            {
-                Some( f ) => println!( "Part 2 Result: {}", f( &input_str ) ),
-                None => println!( "No function for part 2" )
-            };
+            println!( "Part 1 Result: {}", (day_info.part1_func)( &input_str ) );
+            println!( "Part 2 Result: {}", (day_info.part2_func)( &input_str ) );
 
             return Ok( () );
         },
         None =>
         {
-            println!( "No input defined for day {}", which_day );
+            println!( "Day {} undefined", which_day );
             return Err( -2 );
         }
     }
